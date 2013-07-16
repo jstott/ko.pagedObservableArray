66d6b922df4fa4eb1abf30de42f4ce7bdfbe42f6
jstott <jwstott@gmail.com>
2013-07-16 16:26:15 -0400

    added grunt build/minify

---
+++ gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: '<%= pkg.name %>.js',
+        dest: '<%= pkg.name %>.min.js'
+      }
+    },
+	jshint: {
+		options: {
+			jshintrc: '.jshintrc'
+		},
+		all: ['<%= pkg.name %>.js']
+	},
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // task(s).
+  grunt.registerTask('syntax', ['jshint']);
+  grunt.registerTask('default', ['jshint','uglify']);
+
+};
